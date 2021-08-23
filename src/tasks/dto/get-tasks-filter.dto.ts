fc4a1b3735506b54d84fb454298245a4cdb8207d
Emiliano Mazzaglia <emiliano.mazzaglia@rootstrap.com>
2021-08-23 17:44:35 -0300

    Add postgres integration, typeorm, and class validator

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
-import { TaskStatus } from '../task.model';
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
+import { TaskStatus } from '../task-status.enum.ts';
 
 export class GetTasksFilterDto {
-  status: TaskStatus;
-  search: string;
+  @IsOptional()
+  @IsEnum(TaskStatus)
+  status?: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
+  search?: string;
 }
